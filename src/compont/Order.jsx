be3f233874dd8c077bcf7596e37aae8f9a541d39
Usama-Abbasi0001 <157971981+Usama-Abbasi0001@users.noreply.github.com>
2024-05-19 07:06:30 +0500

    react-02

---
+++ src/compont/Order.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Para from "./Para";
+
+function Order(){
+
+    return(
+        <>
+        <ol>
+      <li>Huzaifa</li>
+      <li>Azam</li>
+      <li>Rehman</li>
+      <li>Asim</li>
+      <li>Huzaifa</li>
+    </ol>
+    <Para />
+    </>
+    );
+}
+export default Order;
---
+++ src/compont/Para.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Para(){
+    return (
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem molestiae suscipit minima est. Fugit corrupti inventore fugiat officiis omnis ab. Rem debitis itaque pariatur ab fugiat quae quo accusantium. Ullam.</p>
+    );
+}
+export default Para;
